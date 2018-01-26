ee201a25847206f9bec3e7ee519fdc8b81da91d2
jRob <robinson.jo@husky.neu.edu>
2018-01-26 18:18:07 -0500

    added printouts to demo

--- demo/demo_parse_vicon.m
+++ demo/demo_parse_vicon.m
@@ -1,4 +1,11 @@
 %% demo: parse single vicon file as exported from system.
+%     AUTHOR    : Joseph Robinson
+%     DATE      : January-2018
+%     Revision  : 1.0
+%     DEVELOPED : 2017b
+%     FILENAME  : parse_vicon.m
+%
+disp('Demo: BEGIN')
 rdir = mocap_setup();
 obins = strcat(rdir, '/demo/results/', {'emg/', 'skeleton/'});
 fin = [rdir '/data/Allyson-1-devel.csv'];
@@ -8,6 +15,15 @@ fbase = utils.basename(fin);
 cellfun(@utils.checkdir, obins)
 % parse vicon data
 [emg_table, skel_table] = parse_vicon(fin);
+disp('EMG DATA:')
+disp(head(emg_table))
+disp('')
+
+disp('SKELETON DATA:')
+disp(head(skel_table))
+
+
 writetable(emg_table, [obins{1}, fbase], 'Delimiter',',');
 
 writetable(skel_table, [obins{2}, fbase], 'Delimiter',',');
+disp('Demo: END')
