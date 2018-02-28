a3bd95d6f7d49cced4ac0d83d93fc2f12321fa51
jRob <robinson.jo@husky.neu.edu>
2018-02-28 13:46:42 -0500

    wip

--- include/@ViconSkeleton/ViconSkeleton.m
+++ include/@ViconSkeleton/ViconSkeleton.m
@@ -1,7 +1,8 @@
-classdef ViconSkeleton 
+classdef ViconSkeleton
     %VICONSKELETON Summary of this class goes here
     %   Detailed explanation goes here
     
+  
     properties
         RFHD    % right front head
         LBHD    % left back head
@@ -44,6 +45,11 @@ classdef ViconSkeleton
         RANK    % right ankle
         RHEE    % right heel
         RTOE    % right toe
+        
+        nparts;
+        part_strings;
+        markers;        % cell array of names and indices TODO
+        nframes;
     end
     
     methods
@@ -57,15 +63,16 @@ classdef ViconSkeleton
             uparts = fieldnames(obj);
             
             
-            nparts = length(uparts);
-            
-            for x = 1:nparts
+            obj.nparts = length(uparts);
+            obj.nframes = size(obj.RTOE,1);
+            for x = 1:obj.nparts
                 part = uparts{x};
                 ids = strcmp(part, parts);
                 coords = cell2mat(cell_data(:,ids));
                 obj.(part) = coords;
             end
-            
+            obj = obj.set_nframes();
+            obj = obj.set_nparts();
             %             cellfun(@(x) x(1:end-2), T.Properties.VariableNames,'uni',false);
         end
         function cl_out = findAttrValue(obj,attrName,varargin)
@@ -94,9 +101,9 @@ classdef ViconSkeleton
         function print(obj, frame_id)
             if nargin < 2, frame_id = 1;    end
             fields = fieldnames(obj);
-            nfields = length(fields);
+            obj.nfields = length(fields);
             str_disp = cell(1, nfields);
-            for x = 1:nfields
+            for x = 1:obj.nfields
                 att = fields{x};
                 vals = obj.(att);
                 str_disp{x} = sprintf('%s: (%d, %d, %d)\n', att, ...
@@ -105,10 +112,97 @@ classdef ViconSkeleton
             fprintf([str_disp{:} '\n\n']);
             %             sprintf(
         end
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        
+        function show(obj)
+            fields = fieldnames(obj);
+            obj.nparts = length(fields);
+            % Overlay the skeleton on this RGB frame.
+            for i = 1:obj.nparts
+                for body = 1:nBodies
+                    X1 = [colorJointIndices(SkeletonConnectionMap(i,1),1,body) colorJointIndices(SkeletonConnectionMap(i,2),1,body)];
+                    Y1 = [colorJointIndices(SkeletonConnectionMap(i,1),2,body) colorJointIndices(SkeletonConnectionMap(i,2),2,body)];
+                    line(X1,Y1, 'LineWidth', 1.5, 'LineStyle', '-', 'Marker', '+', 'Color', colors(body));
+                    %         imgOut = insertShape(imgOut,'Line',[X1(1) X1(2) Y1(1) Y1(2)],'LineWidth',5,'Color',colors(body));
+                    hold on;
+                end
+                hold off;
+            end
+        end
+        
+        function obj = set_nparts(obj)
+            sensor_names = fieldnames(obj);
+            
+            % determine field names pointing to parts-- part references are
+            % all caps
+            parts_ids=cell2mat(cellfun(@(str) any(isstrprop(str,'upper')...
+                ), sensor_names, 'uni', false));
+            
+            sensor_names = sensor_names(parts_ids);
+            obj.nparts = length(sensor_names);
+            obj.part_strings =  sensor_names;
+            
+        end
+        function obj = set_nframes(obj)
+            obj.nframes = size(obj.C7, 1);
+        end
+        
+        function nnans = count_nans(obj, part)
+            % count the number of nans found in a matrix. Note that matrix
+            % is assumed to be made up of samples (i.e., one unique feature
+            % row, with columns seperating feature dimensions).
+            
+            miss_matrix = isnan(obj.(part));
+        end
+        function [obj, count_incomplete_parts]  = ismissing(obj)
+            %              ismissing(obj.RASI)
+            obj = obj.set_nparts();
+            obj = obj.set_nframes();
+            
+            count_incomplete_parts = zeros(1, obj.nparts);
+            for x = 1:obj.nparts
+                miss_matrix = isnan(obj.(obj.part_strings{x}));
+                % sum across the rows
+                % i.e., this will be 3 for samples x, y, and z = nan, 1 if a
+                % single channel is nan, 0 if none are nan (i.e., coords for
+                % ith sample were properly/ completely captured).
+                sample_counts = sum(miss_matrix,2);
+                %
+                count_incomplete_parts(x) = sum(sample_counts > 0);
+            end
+            
+            
+        end
+        
+        function parts = get_parts_str(obj)
+            % returns cell array of members of instance of ViconSkeleton
+            parts = obj.part_strings;
+        end
+        
+        function fhandle = scatter_plot(obj, frame_step)
+            %  plots skeleton as scatter plot in euclean space 
+            % 'frame_step' is optional argument allowing for the number of
+            % frames skipped between axis views to be altered (default 100)
+            
+            if nargin < 2
+                frame_step = 100;
+            end
+            parts = obj.get_parts_str();            
+            fhandle = figure();
+            
+            for x = 1:frame_step:obj.nframes
+                % for each frame (i.e., 100 fps captured by vicon)
+                hold on;
+                for r = 1:obj.nparts
+                    % for each marker 
+                    fprintf(1, 'adding part %s to scatter plot\n', parts{r});
+                    
+                    coords = obj.(parts{r})(x,:);
+                    scatter3(coords(1), coords(2), coords(3));
+                end
+                view(45,45)
+                pause(1)
+                clf(fhandle)
+            end
         end
     end
 end
--- scripts/run_vicon_parser.m
+++ scripts/run_vicon_parser.m
@@ -4,28 +4,45 @@
 %     Revision  : 1.0
 %     DEVELOPED : 2017b
 %     FILENAME  : run_vicon_parser.m
-d1 = dir('../Vicon_Nexus_data/*/*.csv');
-obin = '../parsed_vicon/';
+d1 = dir('../Vicon_data_csv_Feb10/*.csv');
+obin = '../parsed_vicon_new/';
 obins = strcat(obin, {'emg/', 'skeleton/'});
 
+% make output directories
 cellfun(@utils.checkdir, obins)
 
+% prepare filepaths for skeleton and emg data extracted from each file
 nfiles = length(d1);
 fpaths = strcat({d1.folder}, '/', {d1.name});
 fout_emg = strcat(obins{1}, {d1.name});
 fout_skel = strcat(obins{2}, {d1.name});
 
+
+f_skeleton_exists = cellfun(@exist,fout_skel);
+f_emg_exists = cellfun(@exist,fout_emg);
+
+do_exist =  f_skeleton_exists | f_emg_exists;
+
 parfor x = 1:nfiles
-    % each vicon file
+    %% each vicon file
     
-    if exist(fout_emg{x},'file')
+    if do_exist(x)
+        %% if either exist, skip
+        fprintf('\nNot processing %s\n', fpaths{x})
+        fprintf('\n%s exists: (%d)\n%s exists: (%d)\n', fout_skel{x}, ...
+            f_skeleton_exists(x), fout_emg{x}, f_emg_exists(x));
+        
         continue
     end
     try
+        % try-catch 
         [emg_table, skel_table] = parse_vicon(fpaths{x});
+        
+        % write-out parsed tables
         writetable(emg_table, fout_emg{x}, 'Delimiter',',');
         writetable(skel_table, fout_skel{x}, 'Delimiter',',');
     catch e
+        % print warning if exception thrown
         fprintf(1, 'WARNING: [%d] %s thrown exception', x, fpaths{x});
     end
     
